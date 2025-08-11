610fa71af59e7fcd7ab4ea3afd2d214c1be27c3d
wst7 <wstreet7@outlook.com>
2025-08-11 16:21:50 +0800

    fix: debugger

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: process.env.LOG_LEVEL == 'debug',
   providers: [GitHub],
   trustHost: true,
 });
